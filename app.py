98eb6826315009d93fb6947bdfaeec43eb31b9aa
Dasun <techsaralk.pro@gmail.com>
2024-06-02 23:29:53 +0530

    flask app initialized

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 # imports
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 # custom imports
 from APICaller import get_characters
 
+# flask app initialization
+app: Flask = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
-    get_characters()
+    # get_characters()
+    app.run(host="localhost", port=8080, debug=True)
